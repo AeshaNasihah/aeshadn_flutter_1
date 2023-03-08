2b0e032ef1ee540c6b30048e9a2b209cca3275e1
aesha durratul nasihah <aeshanasihah28@gmail.com>
2023-03-08 16:26:45 +0700

    update

--- lib/Explore_pages.dart
+++ lib/Explore_pages.dart
@@ -32,108 +32,74 @@ class ExplorePage extends StatelessWidget {
                       style: GoogleFonts.poppins(
                           fontSize: 18, fontWeight: FontWeight.w600)),
                   const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 245, 244, 247),
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Stack(
-                              children: [
-                                Image.asset(
-                                  "assets/images/image_6.png",
-                                  height: 80,
-                                  width: 120,
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for (var i = 0; i < 5; i++)
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 245, 244, 247),
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Stack(
+                                  children: [
+                                    Image.asset(
+                                      "assets/images/image_6.png",
+                                      height: 80,
+                                      width: 120,
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.only(
+                                          left: 5, top: 10),
+                                      padding: const EdgeInsets.all(4),
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xff322E4A),
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Text(
+                                        "New",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.normal,
+                                          color: const Color(0xFFF7AF4B),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                Container(
-                                  margin:
-                                      const EdgeInsets.only(left: 5, top: 10),
-                                  padding: const EdgeInsets.all(4),
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xff322E4A),
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Text(
-                                    "New",
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.normal,
-                                      color: const Color(0xFFF7AF4B),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(6),
+                                    child: Text(
+                                      "Lagon Sky",
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(6),
-                                child: Text(
-                                  "Lagon Sky",
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
+                                  Container(
+                                    padding: const EdgeInsets.all(6),
+                                    child: Text(
+                                      "USD 920 ",
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.normal,
+                                        color: Color(0xffF7AF4B),
+                                      ),
+                                      textAlign: TextAlign.end,
+                                    ),
                                   ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(6),
-                                child: Text(
-                                  "USD 920 ",
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.normal,
-                                    color: Color(0xffF7AF4B),
-                                  ),
-                                  textAlign: TextAlign.end,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(6),
-                            child: Text(
-                              "800 sq ft.",
-                              style: GoogleFonts.poppins(
-                                fontSize: 12,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 245, 244, 247)),
-                            child: Image.asset(
-                              "assets/images/image_8.png",
-                              height: 80,
-                              width: 120,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(6),
-                                child: Text(
-                                  "Inn Parapat",
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
+                                ],
                               ),
                               Container(
                                 padding: const EdgeInsets.all(6),
@@ -146,10 +112,9 @@ class ExplorePage extends StatelessWidget {
                                 ),
                               ),
                             ],
-                          )
-                        ],
-                      )
-                    ],
+                          ),
+                      ],
+                    ),
                   ),
                 ],
               ),
